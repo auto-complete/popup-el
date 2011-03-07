93a6cce649d41109e48d78f12eacdd5f505bd453
Tomohiro Matsuyama <tomo@cx4a.org>
2011-03-07 18:19:54 +0900

    Added commentary.

--- popup.el
+++ popup.el
@@ -21,7 +21,10 @@
 
 ;;; Commentary:
 
-;; 
+;; popup.el is a visual popup user interface library for Emacs. This
+;; provides a basic API and common UI widgets such as popup tooltips
+;; and popup menus.
+;; See README.markdown for more information.
 
 ;;; Code:
 
