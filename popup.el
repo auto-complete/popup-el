eaa7f30a66945c8ea27fb5acc0835cab3bf10e88
Kiyoka Nishiyama <kiyoka@sumibi.org>
2014-08-13 23:02:43 +0900

    Factoring:   1) Set to default value of initial-index argument is nil.   2) rewrite range-check of initial-index.   3) rewrite description.

--- popup.el
+++ popup.el
@@ -1317,7 +1317,7 @@ PROMPT is a prompt string when reading events during event loop."
                        (isearch-cursor-color popup-isearch-cursor-color)
                        (isearch-keymap popup-isearch-keymap)
                        isearch-callback
-		       (initial-index 0)
+		       initial-index
                        &aux menu event)
   "Show a popup menu of LIST at POINT. This function returns a
 value of the selected item. Almost arguments are same as
@@ -1353,7 +1353,8 @@ ISEARCH-CALLBACK is a function taking one argument.  `popup-menu'
 calls ISEARCH-CALLBACK, if specified, after isearch finished or
 isearch canceled. The arguments is whole filtered list of items.
 
-INITIAL-INDEX is a initial index value for `select-menu' function."
+If `INITIAL-INDEX' is non-nil, this is an initial index value for
+`popup-select'. Only positive integer is valid."
   (and (eq margin t) (setq margin 1))
   (or margin-left (setq margin-left margin))
   (or margin-right (setq margin-right margin))
@@ -1383,10 +1384,7 @@ INITIAL-INDEX is a initial index value for `select-menu' function."
           (popup-draw menu))
 	(when initial-index
 	  (popup-select menu
-			(let ((end-index (- (length list) 1)))
-			  (if (< end-index initial-index)
-			      end-index
-			    initial-index))))
+			(min (- (length list) 1) initial-index)))
         (if nowait
             menu
           (popup-menu-event-loop menu keymap fallback
