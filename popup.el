9d104d4bbbcb37bbc9d9ce762e74d41174683f86
Jen-Chieh <jcs090218@gmail.com>
2020-06-10 11:17:32 +0800

    Bump version.

--- popup.el
+++ popup.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tomohiro Matsuyama <m2ym.pub@gmail.com>
 ;; Keywords: lisp
-;; Version: 0.5.7
+;; Version: 0.5.8
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -31,7 +31,7 @@
 
 (require 'cl-lib)
 
-(defconst popup-version "0.5.7")
+(defconst popup-version "0.5.8")
 
 
 
